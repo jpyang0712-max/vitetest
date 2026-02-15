96020b22e085094d7d14ef433e95fa8c076b2c76
jpyang0712-max <jpyang0712@gmail.com>
2026-02-15 23:01:17 +0900

    Initial commit: Vite project

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
+
+export default defineConfig({
+  // 빌드 결과물이 나올 폴더 지정 (기존처럼 theme로 하고 싶다면)
+  build: {
+    outDir: 'theme',
+  },
+  plugins: [
+    handlebars({
+      // @@include 대신 {{> filename}} 문법을 사용하게 됩니다.
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }),
+  ],
+});
